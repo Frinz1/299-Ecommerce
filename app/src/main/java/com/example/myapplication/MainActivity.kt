01c62a42c027942a25bd0920b088bb8aee68190d
FRINZ CHARLES <charles.biasora@gmail.com>
2023-10-15 16:09:24 +0800

    lab u

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/myapplication/activity_main_splashscreen.kt
+++ app/src/main/java/com/example/myapplication/activity_main_splashscreen.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
-@Suppress("DEPRECATION")
+
 class activity_main_splashcreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_splashscreen)
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
